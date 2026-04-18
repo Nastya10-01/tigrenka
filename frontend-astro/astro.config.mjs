26345a01e765564e28108c94c2b22bb846b88189
nasver <anastasiisirsova@gmail.com>
2026-04-18 20:19:12 +0500

    конфигур

--- frontend-astro/astro.config.mjs
+++ frontend-astro/astro.config.mjs
@@ -1,10 +1,13 @@
-import { defineConfig } from 'astro/config'
+import { defineConfig } from 'astro/config' // Функция для создания конфигурации Astro
 import node from '@astrojs/node'
 import { fileURLToPath } from 'url'
 
 export default defineConfig({
+  // Страницы генерируются на сервере при каждом запросе
   output: 'server',
   adapter: node({ mode: 'standalone' }),
+
+  // Настройки Vite (сборщик, который использует Astro)
   vite: {
     resolve: {
       alias: {
